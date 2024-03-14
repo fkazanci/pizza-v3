f901a7f97c0a32ef03407572ec5e1dce7d3df2d9
fkazanci <fkazanci@gmail.com>
2024-03-14 11:26:18 +0300

    deneme

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Switch, Route} from "react-router-dom";
 import './App.css'
 
+import OrderPizza from "./Components/OrderPizza";
+import MainPage from './Components/MainPage';
+import Success from "./Components/Success";
+import React, { useState} from "react";
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [orderData, setOrderData] = useState(null);
+
+  const handleOrderData = (d) => {
+    setOrderData(d);
+  };
 
   return (
     <>
-      <p>deneme</p>
+      <Switch> 
+        <Route exact path="/"  >
+          <MainPage />
+        </Route>
+        <Route path="/order"  >
+          <OrderPizza onSubmit={handleOrderData}/>
+        </Route>
+        <Route path="/success">
+          <Success orderData={orderData} />
+        </Route>
+      </Switch>
     </>
-  )
+    );
 }
 
 export default App
